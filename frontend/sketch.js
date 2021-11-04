3b5d2fc9badba1fa5010698364e982e2070698fe
Thomas <thomas.juldo@edu.devinci.fr>
2021-11-04 17:53:27 +0100

    Switching to Ok sign

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -96,7 +96,7 @@ function selection(){
     if (hands.left_hand.hand_pose_t[8] !== undefined &&
         hands.right_hand.hand_pose_t[8] !== undefined &&
         "left_hand_sign" in global_data) {
-        if (global_data["left_hand_sign"][0] == "OPEN_HAND" && global_data["left_hand_sign"][1] > 0.8 && !dance.dance.init)
+        if (global_data["left_hand_sign"][0] == "OK" && global_data["left_hand_sign"][1] > 0.8 && !dance.dance.init)
         {
             selector.display_bubbles = true;
         }
